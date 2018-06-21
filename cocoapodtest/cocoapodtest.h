7f6092aa34afa0f88c680149f04a438a7b1c1db9
Olivier Lepage <olivierlepage@protonmail.com>
2018-06-21 09:28:03 -0400

    Initial Commit

---
+++ cocoapodtest/cocoapodtest.h
@@ -0,0 +1,19 @@
+//
+//  cocoapodtest.h
+//  cocoapodtest
+//
+//  Created by Olivier Lepage on 2018-06-21.
+//  Copyright © 2018 Olivier Lepage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for cocoapodtest.
+FOUNDATION_EXPORT double cocoapodtestVersionNumber;
+
+//! Project version string for cocoapodtest.
+FOUNDATION_EXPORT const unsigned char cocoapodtestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cocoapodtest/PublicHeader.h>
+
+
